7876858f2831847f076ddddfa8857f853891e62e
vararais <varadigmaisnann@gmail.com>
2024-05-14 19:18:00 +0700

    Membuat Chain Function

---
+++ ChainFunction1/ChainFunction1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class buku {
+    string judul;
+public:
+    buku setJudul(string judul) {
+        this->judul = judul;
+        return *this;
+    }
+    string getJudul() {
+        return this->judul;
+    }
+}; bukunya;
+
+int main()
+{
+    //bukunya.setJudul("Matematika");
+    //cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul();
+    return 0;
+
+}
