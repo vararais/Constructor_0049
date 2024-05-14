6af445883db41b6234d6ff4a7fb15c77a3f5fdfa
vararais <varadigmaisnann@gmail.com>
2024-05-14 18:27:12 +0700

    Memanggil Int dan Void

--- Static2/Static2.cpp
+++ Static2/Static2.cpp
@@ -21,6 +21,18 @@ public:
     }
 };
 
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+    id = ++nim;
+}
+
+void mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
 int main()
 {
     
