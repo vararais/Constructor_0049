cbffc0dd0abf9a71fa39e49ddbca6af50176d840
vararais <varadigmaisnann@gmail.com>
2024-05-14 18:17:17 +0700

    Menambah Access Modifier dan void

--- Static2/Static2.cpp
+++ Static2/Static2.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 class mahasiswa {
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
 
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; }
+    static int getNim() { return nim; }
 };
 
 int main()
